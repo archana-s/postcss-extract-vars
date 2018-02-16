9ee69331af41cf1439a1fe2a116867398ab94c9e
Archana Sankaranarayanan <asankar@netflix.com>
2018-02-16 14:43:24 -0800

    First set of files

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-var postcss = require('postcss');
+const postcss = require('postcss');
+const fs =  require('fs');
 
-module.exports = postcss.plugin('PLUGIN_NAME', function (opts) {
+/* eslint-disable quotes */
+module.exports = postcss.plugin('postcss-extract-vars', function (opts) {
     opts = opts || {};
+    const outputFile = opts.file;
 
-    // Work with options here
-
-    return function (root, result) {
-
-        // Transform CSS AST here
-
+    return function (css) {
+        let outputCSS = `:root {`;
+        css.walkRules(function (rule) {
+            if (rule.selector === ':root') {
+                rule.nodes.forEach(node => {
+                    if (node.prop && node.value) {
+                        outputCSS = `${outputCSS}
+                            ${node.prop}: ${node.value};`;
+                    }
+                });
+            }
+        });
+        outputCSS = `${outputCSS}
+            }`;
+        if (outputFile) {
+            fs.writeFile(outputFile, outputCSS, (err) => {
+                if (err) {
+                    throw new Error(
+                        `Could not write to file ${outputFile}: ${err}`
+                    );
+                }
+            });
+        }
     };
+
 });
--- index.test.js
+++ index.test.js
@@ -1,19 +1,38 @@
 var postcss = require('postcss');
+var fs = require('fs');
 
 var plugin = require('./');
 
 function run(input, output, opts) {
     return postcss([ plugin(opts) ]).process(input)
-        .then(result => {
-            expect(result.css).toEqual(output);
-            expect(result.warnings().length).toBe(0);
+        .then(() => {
+            setTimeout(() => {
+                const outputFile = opts.file;
+                const expectedOutput =
+                    fs.readFileSync(output, 'utf8').replace(/\s/, '');
+                const outputData =
+                    fs.readFileSync(outputFile, 'utf8').replace(/\s/, '');
+                expect(outputData).toEqual(expectedOutput);
+            }, 2000);
         });
 }
 
-/* Write tests here
-
-it('does something', () => {
-    return run('a{ }', 'a{ }', { });
+it('generates a vars file with root variables', () => {
+    return run(':root{ --color-red: red; --color-blue: blue; }',
+        './tests/expectedOutput1.css',
+        { file: './tests/output1.css' });
 });
 
-*/
+it('creates empty file for no vars', () => {
+    return run('.colored {color: red; }',
+        './tests/expectedOutput2.css',
+        { file: './tests/output2.css' });
+});
+
+it('does not include variables not set in root scope', () => {
+    return run(`:root {--color-red: red; --color-blue: blue;}.
+        button { --color: red; }`,
+    './tests/expectedOutput3.css',
+    { file: './tests/output3.css' });
+});
+
